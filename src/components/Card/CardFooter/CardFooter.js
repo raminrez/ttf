e19bb147ddee127627667a6435dbed75b6b0c3de
raminrez <ramin12207@gmail.com>
2019-09-04 19:32:21 +0430

    set addtocard function

--- src/components/Card/CardFooter/CardFooter.js
+++ src/components/Card/CardFooter/CardFooter.js
@@ -1,16 +1,24 @@
 /* eslint-disable */
 import React from "react";
+import { connect } from "react-redux";
+import { doAddToCard, doRemoveFromCard } from "../../../redux/actions/card";
 
-const CardFooter = ({ price }) => {
+const CardFooter = ({ price, createdAt, addToCard, removeFromCard }) => {
   return (
     <footer className='card-footer'>
       <div className='card-footer-item '>
         <a
-          href='#'
+          onClick={event => handleClick(event, createdAt, addToCard)}
           className='button is-fullwidth is-success has-text-weight-light'
         >
-          همین حالا خرید کن
+          خرید
         </a>
+        {/* <a
+          onClick={event => handleClick2(event, createdAt, removeFromCard)}
+          className='button is-fullwidth is-success has-text-weight-light'
+        >
+          remove
+        </a> */}
       </div>
       <p className='card-footer-item has-text-success has-text-right is-unselectable'>
         {`  ${price} ت `}
@@ -19,8 +27,26 @@ const CardFooter = ({ price }) => {
   );
 };
 
+const handleClick = (e, createdAt, addToCard) => {
+  e.preventDefault();
+  addToCard(createdAt);
+};
+
+// const handleClick2 = (e, createdAt, removeFromCard) => {
+//   e.preventDefault();
+//   removeFromCard(createdAt);
+// };
+
+const mapDispatchToProps = dispatch => ({
+  addToCard: createdAt => dispatch(doAddToCard(createdAt))
+  // removeFromCard: createdAt => dispatch(doRemoveFromCard(createdAt))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CardFooter);
+
 CardFooter.defaultProps = {
   price: 16900
 };
-
-export default CardFooter;
