63f9e57436853a1be986dc541906944213d77c2c
raminrez <ramin12207@gmail.com>
2019-09-04 19:34:00 +0430

    set cardReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import productReducer from "./product";
+import cardReducer from "./card";
 
 const rootReducer = combineReducers({
-  productState: productReducer
+  productState: productReducer,
+  cardState: cardReducer
 });
 
 export default rootReducer;
