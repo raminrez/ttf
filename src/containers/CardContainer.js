d8d2008c78cd064ef18fb1783a3f172b08291eaf
raminrez <ramin12207@gmail.com>
2019-09-04 19:32:30 +0430

    fix break points

--- src/containers/CardContainer.js
+++ src/containers/CardContainer.js
@@ -6,9 +6,16 @@ const CardContainer = ({ products }) => {
     <section className='section'>
       <div className='container'>
         <div className='columns is-multiline'>
-          {products.map(({ title, desc, id, price, quantity }) => (
-            <Card title={title} subtitle={desc} />
-          ))}
+          {(products || []).map(
+            ({ title, desc, id, price, quantity, createdAt }) => (
+              <Card
+                key={createdAt}
+                createdAt={createdAt}
+                title={title}
+                subtitle={desc}
+              />
+            )
+          )}
         </div>
       </div>
     </section>
