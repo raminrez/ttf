0084ab3e5f92a32870b7d33d6a9a224c9c1e0883
raminrez <ramin12207@gmail.com>
2019-09-04 16:08:22 +0430

    Class Based

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import "./App.scss";
 import CardContainer from "./containers/CardContainer";
 import Toolbar from "./components/Toolbar/Toolbar";
+import { doFetchProducts } from "./redux/actions/product";
+import { getAllProducts } from "./redux/selectors/product";
 
-function App() {
-  return (
-    <>
-      <Toolbar />
-      <CardContainer />
-    </>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.onFetchProducts();
+  }
+
+  render() {
+    console.log(this.props.products);
+    return (
+      <>
+        <Toolbar />
+        <CardContainer />
+      </>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  products: () => getAllProducts(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  onFetchProducts: () => dispatch(doFetchProducts())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
