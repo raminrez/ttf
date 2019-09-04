507a88520182b7126fec2689fe210cddc5996937
raminrez <ramin12207@gmail.com>
2019-09-04 19:31:29 +0430

    pass params

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,13 +3,13 @@ import CardImage from "./CardImage/CardImage";
 import CardContent from "./CardContent/CardContent";
 import CardFooter from "./CardFooter/CardFooter";
 
-const Card = ({ title, subtitle }) => {
+const Card = ({ title, subtitle, createdAt }) => {
   return (
     <div className='column is-one-third'>
       <div className='card'>
         <CardImage />
         <CardContent title={title} subtitle={subtitle} />
-        <CardFooter />
+        <CardFooter createdAt={createdAt} />
       </div>
     </div>
   );
